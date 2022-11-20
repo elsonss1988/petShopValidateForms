97e82b7b1979d2b27d46c5714147a6370a03e44f
Elson Santos <>
2022-11-20 13:04:55 -0300

    Validores de Forms

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Doguito 
 
-Validação de formulários com de PetShop
+Projeto de PetShop com validação de formulário
 
 * Regex e uso de Types em HTML
+* App Via CEP
+* SimpleMaskMoney
+* Customização de erros
---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+import {valida} from './validate.js'
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach((input)=>{
+    if(input.dataset.tipo === 'preco'){
+        SimpleMaskMoney.setMask(input, {
+            afterFormat(e) { console.log('afterFormat', e); },
+            allowNegative: false,
+            beforeFormat(e) { console.log('beforeFormat', e); },
+            negativeSignAfter: false,
+            prefix: 'R$',
+            suffix: '',
+            fixed: true,
+            fractionDigits: 2,
+            decimalSeparator: ',',
+            thousandsSeparator: '.',
+            cursor: 'end'
+    })
+
+    input.addEventListener('blur',(evento)=>{
+        valida(evento.target)
+    })
+}})
